6db7db4adae28bfae61a7ab1dfaa27359301e243
MSK-1512 <168532885+MSK-1512@users.noreply.github.com>
2024-05-31 22:46:19 +0900

    課題提出：DOM操作を使用して文字表示を変えよう

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', () => {
+    const text = document.getElementById('text');
+    text.textContent = 'ボタンをクリックしました';
+});
